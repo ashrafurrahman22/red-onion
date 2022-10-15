0255b9971e45849cac69f3c53732178fe67a253a
ASHRAFUR RAHMAN <you@example.com>
2022-10-15 11:56:54 +0600

    navbar-added

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>This is HomePage</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem mollitia deserunt, incidunt iste fugiat, ipsum numquam corporis sit assumenda voluptates, ullam perspiciatis quos architecto illo in soluta earum aspernatur harum?</p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Shared/Navbar.js
@@ -0,0 +1,51 @@
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo2.png'
+
+const Navbar = () => {
+    return (
+        <div style={{
+          fontFamily:"poppins"
+        }} className="navbar bg-base-100 py-2">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Item 1</a></li>
+        <li tabIndex={0}>
+          <a className="justify-between">
+            Parent
+            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
+          </a>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+    <a className="btn w-44 btn-ghost normal-case text-xl">
+      <img src={logo} alt="" />
+    </a>
+  </div>
+  <div className="navbar-end hidden lg:flex">
+    <ul className="menu menu-horizontal p-0">
+      <li><a><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+      </a></li>
+      <li><Link to='/login'>Login</Link></li>
+      <li ><Link to='/signup'>
+        <button
+         className='btn bg-red-600 w-28 text-white font-extralight normal-case rounded-full'>Sign up</button>
+        </Link></li>
+    </ul>
+  </div>
+</div>
+    );
+};
+
+export default Navbar;
