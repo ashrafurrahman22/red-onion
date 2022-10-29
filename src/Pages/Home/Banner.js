5baec8a329af376d1f8091e91a5a33b4b787c324
ASHRAFUR RAHMAN <you@example.com>
2022-10-29 10:50:19 +0600

    updated

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -30,23 +30,21 @@ const Banner = () => {
             </div>
 
             {/* <div className='relative lg:top-[-340px] top-[-120px]'> */}
-            <div className='absolute lg:top-[230px] left-[360px]'>
-            <h2 className='lg:text-4xl font-semibold text-center'>Best Food Waiting for your Belly</h2>
-            <div className='text-center py-6'>
-            {/* <form onSubmit={handleSearch}>
-            <input className='border border-black lg:py-2 lg:px-10 rounded-full mr-2' type="text" placeholder='Search' name='search' />
-            <input className='btn bg-red-600 lg:w-28  text-white lg:font-medium normal-case rounded-full' type="submit" value="Search" />
-            </form> */}
+            <div className='absolute lg:top-[230px] top-[80px] lg:left-[360px] left-[95px]'>
+            <h2 className='lg:text-4xl text-sm font-semibold text-center'>Best Food Waiting for your Belly</h2>
+            <div className='text-center lg:py-6 py-2'>
             <form style={{
         fontFamily:"poppins"
-      }} onSubmit={handleSubmit(onSubmit)}>
-      <input className='border border-black lg:py-2 lg:px-10 rounded-full mr-2' placeholder='Search' {...register("search")} required/>
+      }} onSubmit={handleSubmit(onSubmit)}
+      className="flex lg:flex-row flex-col justify-center"
+      >
+      <input className='border border-black px-4 lg:w-72 w-44 rounded-full mr-2' placeholder='Search' {...register("search")} required/>
       {errors.exampleRequired && <span>This field is required</span>}
-      <input className='btn bg-red-600 lg:w-28  text-white lg:font-medium normal-case rounded-full' type="submit" />
+      <input className='btn lg:btn-xl bg-red-600 lg:w-28 w-32 lg:my-0 my-2 text-white font-medium normal-case rounded-full' type="submit" />
     </form>
             </div>
             </div>
-            <div className='grid grid-cols-3 px-14 gap-10 py-4'>
+            <div className='grid lg:grid-cols-3 px-14 gap-10 py-4'>
                 {
                     // eslint-disable-next-line array-callback-return
                     foods.filter((value) => {
